d03f85c1de8cfa8bfa852b7439e37348aa44dd62
ethanp <ethanp@utexas.edu>
2015-03-30 16:12:03 -0500

    try to get app.sass to work in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
@@ -85,6 +85,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
+
 
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
